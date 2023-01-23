2f32f56c5d880a99b7feba8fdb6c2b8032e752a2
cdewen <96959326+cdewen@users.noreply.github.com>
2023-01-23 11:56:59 -0600

    local working
    working with ngrok and flask to deliver results succesfully for two usernames specifying lxml for beautiful soup so I need to test with others to make sure it can run on any machine

--- src/secTest.py
+++ src/secTest.py
@@ -1,28 +1,24 @@
-import requests
+from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request 
 from bs4 import BeautifulSoup
-import random
+import requests, random
 import re
 
 def createUrl(author: str, page: int) -> str:
-    url = "https://letterboxd.com/" + author + "/watchlist/" + "page/" + str(page)
+    url = "https://letterboxd.com/" + author + "/watchlist/page/" + str(page) + "/"
     return url
 
-def get_parsed_page(url: str) -> None:
-        # This fixes a blocked by cloudflare error i've encountered
-        headers = {
-            "referer": "https://letterboxd.com",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-        }
-
-        return BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
 def getWatchlist(username: str) -> list:
     ret = []
     page = [1]
     maxPage: int = 1
-    pages = get_parsed_page(createUrl(username, 1))
-    pages = pages.find_all("li", {"class":"paginate-page"})
+    req = BeautifulSoup(requests.get(createUrl(username, 1)).text, "lxml")
+    #or can be
+    #req = requests.get(createUrl(username,1))
+    #req = BeautifulSoup(req.content)
+    pageCount = req.find_all("li", {"class":"paginate-page"})
 
-    for item in pages:
+    for item in pageCount:
         if (item.get_text() == '…'):
             pass
         else: 
@@ -32,28 +28,41 @@ def getWatchlist(username: str) -> list:
 
     i = 1
     while i <= maxPage:
-        data = get_parsed_page(createUrl(username, i))
-        data = data.find_all("li", {"class":"poster-container"})
+        print(createUrl(username, i))
+        soup = BeautifulSoup(requests.get(createUrl(username, i)).text, "lxml")
+        #or can be
+        #soup = requests.get(createUrl(username,1))
+        #soup = BeautifulSoup(req.content)
+        data = soup.find_all("li", {"class":"poster-container"})
 
         for item in data:
-            ret.append(item.find("img", {"class":"image"})['alt'])
+            ret.append(str(item.find("img", {"class":"image"})['alt']).encode("ascii", errors="ignore").decode("utf-8"))
         i+=1
 
     return ret
 
-text = input('what are the usernames? ')
+app = Flask(__name__)
+@app.route('/sms', methods=['POST'])
+def send_sms():
+    #incoming message
+    msg = request.values.get("Body").lower()
+    usernames = re.findall("([A-Za-z_0-9.]+)", msg)
 
-usernames = re.findall("([A-Za-z_0-9.]+)", text)
 
+    retOne = getWatchlist(usernames[0])
+    retTwo = getWatchlist(usernames[1])
 
-retOne = getWatchlist(usernames[0])
-retTwo = getWatchlist(usernames[1])
+    #set = set(retOne).intersection(set(retTwo))
+    fin = set(retOne).intersection(set(retTwo))
+    final = list(fin)
 
-set = set(retOne).intersection(set(retTwo))
-final = list(set)
+    randMovie = random.choice(final)
 
-randMovie = final[random.randint(0,len(final))]
+    res = MessagingResponse()
+    res.message(randMovie)
+    return str(res)
 
-print(final)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
