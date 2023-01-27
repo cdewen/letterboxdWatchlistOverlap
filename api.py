dab89a077e321bf4e33549a12074ae731057152f
cdewen <96959326+cdewen@users.noreply.github.com>
2023-01-27 16:25:00 -0600

    pls work

--- api.py
+++ api.py
@@ -8,7 +8,6 @@ import time
 
 
 allMovies = []
-overlap = []
 def createUrl(author: str, page: int) -> str:
     url = "https://letterboxd.com/" + author + "/watchlist/page/" + str(page) + "/"
     return url
@@ -39,7 +38,7 @@ def getMaxPage(username):
 
 def getPageMovies(username: str, maxList: list):
     pageNum = maxList.pop(random.randint(0, len(maxList) - 1))
-    print(createUrl(username, pageNum))
+    #print(createUrl(username, pageNum))
     soup = BeautifulSoup(requests.get(createUrl(username, pageNum)).text, "html.parser")
     data = soup.find_all("li", {"class":"poster-container"})
 
@@ -47,9 +46,12 @@ def getPageMovies(username: str, maxList: list):
         allMovies.append(str(item.find("img", {"class":"image"})['alt']).encode("ascii", errors="ignore").decode("utf-8"))
     return maxList
 
-def getMovie(usernames: list) -> list:
+def getMovie(username: list) -> list:
+    overlap = []
+    overlap.clear()
+    allMovies.clear()
     maxPages = [] #contains the max page for each username
-    for name in usernames:
+    for name in username:
         maxPages.append(getMaxPage(name))
     
     pageLists = [] #list of lists containing the complete list of all usernames pages
@@ -59,37 +61,42 @@ def getMovie(usernames: list) -> list:
     while(len(overlap) < 1 and bool(any(pageLists))):
         for i in range(len(pageLists)):
             if bool(pageLists[i]):
-                pageLists[i] = getPageMovies(usernames[i], pageLists[i])
-        overlap = [item for item, count in collections.Counter(allMovies).items() if count >= len(usernames)]
+                pageLists[i] = getPageMovies(username[i], pageLists[i])
+        overlap = [item for item, count in collections.Counter(allMovies).items() if count >= len(username)]
 
 
     if (len(overlap) > 0):
         randMovie = random.choice(overlap)
     else:
         randMovie = "the users have no overlapping movies in their watchlists"
+    allMovies.clear()
+    overlap.clear()
     return randMovie
 
 app = Flask(__name__)
 @app.route('/sms', methods=['POST'])
 def send_sms():
-    allMovies.clear()
-    overlap.clear()
     start = time.time()
+    allMovies.clear()
     msg = request.values.get("Body").lower()
-    if (msg == 'format'):
+    message = msg.strip
+    if (message == 'format'):
         res = MessagingResponse()
         end = time.time()
+        allMovies.clear()
         res.message("type in any number of usernames seperated by a space or a non-valid character of your choice (ex. / or :) and" 
             + " then send and wait for a movie all users have in their watchlist")
         print(f"sent format in {end - start} seconds")
 
     else:
+        allMovies.clear()
         usernames = re.findall("([A-Za-z_0-9.]+)", msg)
         
         result = getMovie(usernames)
 
         res = MessagingResponse()
         res.message(result)
+        allMovies.clear()
         end = time.time()
         print(f"sent {result} as the random movie between {str(usernames)} in {end - start} seconds")
 
@@ -97,14 +104,14 @@ def send_sms():
 
 @app.route('/test')
 def index():
-    return "brand new" 
+    return "work!!!" 
 
 @app.route('/fail')
 def fail():
     res = MessagingResponse()
     res.message("there is too much traffic. We recommend limiting the number of usernames you send in if you sent more than two or if a user has a watchlist that is more than 40 pages long.")
     print("fail") 
-    return "fail"       
+    return "failure"       
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
