a52e7aa651e0852a4b1a56e1cc7edc9090d1409f
cdewen <96959326+cdewen@users.noreply.github.com>
2023-01-27 11:15:56 -0600

    prod ready

--- api.py
+++ api.py
@@ -4,17 +4,27 @@ from bs4 import BeautifulSoup
 import requests, random
 import re
 import collections
+import time
 
 
-final = []
+allMovies = []
 def createUrl(author: str, page: int) -> str:
     url = "https://letterboxd.com/" + author + "/watchlist/page/" + str(page) + "/"
     return url
 
-def getWatchlist(username: str) -> list:
-    ret = []
-    page = [1]
-    maxPage: int = 1
+def createList(r2):
+    r1=1
+    res = []
+    if (r1 == r2):
+        res.append(r1)
+    else:
+        while(r1 < r2+1 ):
+            res.append(r1)
+            r1 += 1
+    return res
+
+def getMaxPage(username):
+    pageOne = [1]
     req = BeautifulSoup(requests.get(createUrl(username, 1)).text, "html.parser")
     pageCount = req.find_all("li", {"class":"paginate-page"})
 
@@ -22,92 +32,78 @@ def getWatchlist(username: str) -> list:
         if (item.get_text() == '…'):
             pass
         else: 
-            page.append(int(item.get_text()))
+            pageOne.append(int(item.get_text()))
 
-    maxPage = max(page)
+    return max(pageOne)
 
-    i = 1
-    if (maxPage < 20):
-        while i <= maxPage:
-            print(createUrl(username, i))
-            soup = BeautifulSoup(requests.get(createUrl(username, i)).text, "html.parser")
-            data = soup.find_all("li", {"class":"poster-container"})
+def getPageMovies(username: str, maxList: list):
+    pageNum = maxList.pop(random.randint(0, len(maxList) - 1))
+    print(createUrl(username, pageNum))
+    soup = BeautifulSoup(requests.get(createUrl(username, pageNum)).text, "html.parser")
+    data = soup.find_all("li", {"class":"poster-container"})
 
-            for item in data:
-                final.append(str(item.find("img", {"class":"image"})['alt']).encode("ascii", errors="ignore").decode("utf-8"))
-            i+=1
+    for item in data:
+        allMovies.append(str(item.find("img", {"class":"image"})['alt']).encode("ascii", errors="ignore").decode("utf-8"))
+    return maxList
 
+def getMovie(usernames: list) -> list:
+    overlap = []
+    maxPages = [] #contains the max page for each username
+    for name in usernames:
+        maxPages.append(getMaxPage(name))
+    
+    pageLists = [] #list of lists containing the complete list of all usernames pages
+    for num in maxPages:
+        pageLists.append(createList(num))
+    
+    while(len(overlap) < 1 and bool(any(pageLists))):
+        for i in range(len(pageLists)):
+            if bool(pageLists[i]):
+                pageLists[i] = getPageMovies(usernames[i], pageLists[i])
+        overlap = [item for item, count in collections.Counter(allMovies).items() if count >= len(usernames)]
+
+
+    if (len(overlap) > 0):
+        randMovie = random.choice(overlap)
     else:
-        randBits = bool(random.getrandbits(1))
-
-        if randBits:
-            maxPage = 0.5 * maxPage
-            i = 1
-            while i <= maxPage:
-                print(createUrl(username, i))
-                soup = BeautifulSoup(requests.get(createUrl(username, i)).text, "html.parser")
-                data = soup.find_all("li", {"class":"poster-container"})
-
-                for item in data:
-                    final.append(str(item.find("img", {"class":"image"})['alt']).encode("ascii", errors="ignore").decode("utf-8"))
-                i+=1
-        else:
-            i = 0.5 * maxPage
-            while i <= maxPage:
-                print(createUrl(username, i))
-                soup = BeautifulSoup(requests.get(createUrl(username, i)).text, "html.parser")
-                data = soup.find_all("li", {"class":"poster-container"})
-
-                for item in data:
-                    final.append(str(item.find("img", {"class":"image"})['alt']).encode("ascii", errors="ignore").decode("utf-8"))
-                i+=1
-
+        randMovie = "the users have no overlapping movies in their watchlists"
+    overlap.clear()
+    allMovies.clear()
+    return randMovie
 
 app = Flask(__name__)
 @app.route('/sms', methods=['POST'])
 def send_sms():
-    #incoming message
+    start = time.time()
     msg = request.values.get("Body").lower()
     if (msg == 'format'):
         res = MessagingResponse()
+        end = time.time()
         res.message("type in any number of usernames seperated by a space or a non-valid character of your choice (ex. / or :) and" 
             + " then send and wait for a movie all users have in their watchlist")
-        print("sent format")
-    
+        print(f"sent format in {end - start} seconds")
+
     else:
         usernames = re.findall("([A-Za-z_0-9.]+)", msg)
+        
+        result = getMovie(usernames)
 
-        for name in usernames:
-            getWatchlist(name)
-
-        fin = [item for item, count in collections.Counter(final).items() if count >= len(usernames)]
-
-        if (len(fin) < 1):
-            fin.clear()
-            final.clear()
-            res = MessagingResponse()
-            res.message("there are no overlapping movies in the users' watchlists")
-            print("no overlap")
-                
-        else:
-            randMovie = random.choice(fin)
-            fin.clear()
-            final.clear()
-            res = MessagingResponse()
-            res.message(randMovie)
-            print(f"sent {randMovie} as rand movie")
+        res = MessagingResponse()
+        res.message(result)
+        end = time.time()
+        print(f"sent {result} as the random movies in {end - start} seconds")
 
     return str(res)  
 
 @app.route('/test')
 def index():
-    return "changed" 
+    return "brand new" 
 
 @app.route('/fail')
 def fail():
     res = MessagingResponse()
-    res.message("your watchlist is too long just figure it out yourself")
-    print("your watchlist is too long just figure it out yourself") 
+    res.message("there is too much traffic. We recommend limiting the number of usernames you send in if you sent more than two or if a user has a watchlist that is more than 40 pages long.")
+    print("fail") 
     return "fail"       
 
 if __name__ == "__main__":
